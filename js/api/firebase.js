4d9cabc22588c38b636fe0b45befc201b841ac4e
renatozepelinepereira-blip <renatozepelinepereira@gmail.com>
2026-03-11 15:44:54 -0300

    Update firebase.js

--- js/api/firebase.js
+++ js/api/firebase.js
@@ -1,1 +1,16 @@
 
+// js/api/firebase.js - Conexão Centralizada
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+
+const firebaseConfig = { 
+    apiKey: "AIzaSyBA9gyn1dWpSoTD8VORiiPU4hUIEVG7DU8", 
+    authDomain: "sistema-pedidos-3f2c2.firebaseapp.com", 
+    projectId: "sistema-pedidos-3f2c2", 
+    storageBucket: "sistema-pedidos-3f2c2.firebasestorage.app", 
+    messagingSenderId: "669786014126", 
+    appId: "1:669786014126:web:d0da498633a145d56a883f" 
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
